f348e2aee7b2c605a3715eab3a87dbe9872f0438
Boyuan Xu <boyuanxu@usc.edu>
2018-10-14 21:03:39 -0700

    Initial Commit

---
+++ askUSC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  askUSC
+//
+//  Created by Boyuan Xu on 10/14/18.
+//  Copyright © 2018 Boyuan Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
